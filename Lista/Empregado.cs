c1c64d27ce9490bcef2d22daae8178149137022c
Vol-Power <victor.moor@live.com>
2023-05-09 22:29:21 -0300

    Classe empregado

--- Lista/Empregado.cs
+++ Lista/Empregado.cs
@@ -8,5 +8,8 @@ namespace Lista
 {
     internal class Empregado
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
     }
 }
